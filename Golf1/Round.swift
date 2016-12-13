18499c694e2d90317e2d9deb8e462c33b578ef0b
WILARY, BRANDON <775337@ehs-s-232-34>
2016-12-13 10:56:08 -0600

    Added Round class and its constructor.

---
+++ Golf1/Round.swift
@@ -0,0 +1,27 @@
+//
+//  Round.swift
+//  Golf1
+//
+//  Created by WILARY, BRANDON on 12/13/16.
+//  Copyright © 2016 SRIKANTH, NIKHIL. All rights reserved.
+//
+
+import Foundation
+
+class Round {
+    
+    
+    let isPracticeRound = false
+    
+    let score, fairways, penalties, putts, greens, upDownAtt, upDownComp: Int
+    
+    init(score: Int, fairways: Int, penalties: Int, putts: Int, greens: Int, upDownAtt: Int, upDownComp: Int) {
+        self.score = score
+        self.fairways = fairways
+        self.penalties = penalties
+        self.putts = putts
+        self.greens = greens
+        self.upDownAtt = upDownAtt
+        self.upDownComp = upDownComp
+    }
+}
