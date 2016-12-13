abbd88a14be77be6f684a929cd2f16c3ce1da5a2
SRIKANTH, NIKHIL <812425@ehs-s-232-37.local>
2016-12-13 10:47:14 -0600

    Initial Commit

---
+++ Golf1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Golf1
+//
+//  Created by SRIKANTH, NIKHIL on 12/13/16.
+//  Copyright © 2016 SRIKANTH, NIKHIL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
