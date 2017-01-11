1e33bbba2989ca9124b74000acb496c4bef28de2
WILARY, BRANDON <775337@ehs-s-232-34.local>
2017-01-11 10:56:46 -0600

    Added documentation to the constructor and created functions that allow the user to edit the instance variables in case of an input error.

--- Golf1/Round.swift
+++ Golf1/Round.swift
@@ -10,20 +10,57 @@ import Foundation
 
 class Round {
     
+    //practice round boolean that will need to be implemented after the practice round mode is created.
+    private let isPracticeRound: Bool
     
-    let isPracticeRound: Bool
+    //Initialize the class instance variables
+    private var score, fairwayPerc, penalties, putts, greens, upDownPerc: Int
+    private var date: NSDate
     
-    let score, fairways, penalties, putts, greens, upDownPerc: Int
-    let date: NSDate
-    
-    init(score: Int, fairways: Int, penalties: Int, putts: Int, greens: Int, upDownAtt: Double, upDownComp: Double) {
+    /* Constructor for the Round class. Creates a storage device for all match information that the user would require.
+     *
+     * @param int score. This is a user input int that reflects the total amount of shots taken during a round.
+     * @param int fairways. This is a user input int that reflects the total amount of fairways hit during play.
+     * @param int penalties. This is a user input int that reflects the total amount of penalties recieved during play.
+     * @param int putts.  This is a user input int that reflects the total amount of putts taken during the round.
+     * @param int greens.  This is a user input int that reflects the total amount of greens hit during play.
+     * @param int upDownAtt.  This is a user input int that reflects the total amount of up down attempts taken.
+     * @param int upDownComp.  This is a user input int that reflects the total amount of up downs that are completed.
+     */
+    init(score: Int, fairways: Int, penalties: Int, putts: Int, greens: Int, upDownAtt: Int, upDownComp: Int) {
         self.isPracticeRound = false
         self.score = score
-        self.fairways = fairways
+        self.fairwayPerc = Int((Double(fairways) / Double(score)) * 100.0)
         self.penalties = penalties
         self.putts = putts
         self.greens = greens
-        self.upDownPerc = (Int)((upDownComp / upDownAtt) * 100.0)
+        self.upDownPerc = Int((Double(upDownComp) / Double(upDownAtt)) * 100.0)
         self.date = NSDate()
     }
+    
+    //Create a framework that allows the user to change the instance variables in case of a user input error.
+    func setScore(newScore: Int) {
+        self.score = newScore
+    }
+
+    func setPenalties(newPenalties: Int) {
+        self.penalties = newPenalties
+    }
+    
+    func setPutts(newPutts: Int) {
+        self.putts = newPutts
+    }
+    
+    func setGreens(newGreens: Int) {
+        self.greens = newGreens
+    }
+    
+    func setUpDown(newUpDownAtt: Int, newUpDownComp: Int) {
+        self.upDownPerc = Int((Double(newUpDownComp) / Double(newUpDownAtt)) * 100.0)
+    }
+    
+    func setFairwayPerc(newScore: Int, newFairways: Double) {
+        self.fairwayPerc = Int((newFairways / Double(newScore)) * 100.0)
+    }
+    
 }
