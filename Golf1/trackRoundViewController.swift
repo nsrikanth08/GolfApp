8eb965ee5c5b436295dce316084f3f7e444fbef5
KINNY, JASON <803202@ehs-s-232-35>
2017-01-19 10:50:40 -0600

    drop down menu done for weather!!

--- Golf1/trackRoundViewController.swift
+++ Golf1/trackRoundViewController.swift
@@ -11,16 +11,23 @@ import UIKit
 var allMatchInfo = [matchInfo]()
 //Master array that holds all matchInfo objects that are entered
 
-class trackRoundViewController: UIViewController {
+class trackRoundViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet weak var firstName: UITextField!
     @IBOutlet weak var lastName: UITextField!
     @IBOutlet weak var weather: UITextField!
     @IBOutlet weak var location: UITextField!
     
+    var weatherdata = ["Sunny","Cloudy","Rain"]
+    var picker = UIPickerView()
+    
     override func viewDidLoad(){
         super.viewDidLoad()
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(trackRoundViewController.dismissKeyboard)))
+        
+        picker.delegate = self
+        picker.dataSource = self
+        weather.inputView = picker
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,8 +35,23 @@ class trackRoundViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return weatherdata.count
+    }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        weather.text = weatherdata[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return weatherdata[row]
+    }
+    
+
     func dismissKeyboard()
     {
         firstName.resignFirstResponder()
