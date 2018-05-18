8c908432955ce28094af11ef10b70e79f88e9b93
KRUEGER, LILLIAN <755852@desktop-3mhemuk.district196.org>
2018-05-18 11:28:35 -0500

    Implemented the batch export functions.

--- Golf1/View Controllers/previousRoundsViewController.swift
+++ Golf1/View Controllers/previousRoundsViewController.swift
@@ -15,72 +15,13 @@ class previousRoundsViewController: UIViewController, UITableViewDataSource, UIT
     @IBOutlet weak var roundTableView: UITableView!
     var roundList: [Round] = allRounds
     
-    /*func numberOfSections(in TableView: UITableView) -> Int {
-        return 1
-    }
- */
-    
-    
-    //table view will return int for how many rows
-    /*func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return roundList.count
-    }
- */
-    
-    //what is in the table
-    /*func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        
-        let locationName  = roundList[indexPath.row].location
-        let matchDate = roundList[indexPath.row].date
-        let label1 = UILabel(frame: CGRect(x: 15, y: 0, width: cell.frame.width/2, height: cell.frame.height))
-        let label2 = UILabel(frame: CGRect(x: cell.frame.width/2, y: 0, width: cell.frame.width/2 - 20, height: cell.frame.height))
-        label1.text = locationName
-        label2.text = matchDate
-        label2.textAlignment = .right
-        cell.addSubview(label1)
-        cell.addSubview(label2)
-        return cell
-    }
- */
-    
-    
-    /*func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let indexPath = tableView.indexPathForSelectedRow
-        //Index value is manually set to 1 because the index value pressed on the table view is stored here
-        round = roundList[(indexPath?[1])!]
-        performSegue(withIdentifier: "showRound" , sender: self )
-    }
- */
-    
-    /*override func viewDidLoad() {
-        super.viewDidLoad()
-        //roundTableView.reloadData()
-        // Do any additional setup after loading the view.
-
-    }
- */
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    //Enables swipe to delete functionality
-    /*func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
-    {
-        if editingStyle == .delete
-        {
-            roundList.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-            allRounds = roundList
-            saveRounds()
-        }
-        
-    }
- */
     
-    //From doc
+    //From batchExport doc
     
     func numberOfSections(in TableView: UITableView) -> Int {
         return 1
@@ -205,9 +146,6 @@ class previousRoundsViewController: UIViewController, UITableViewDataSource, UIT
         //present popover
         activityView.modalPresentationStyle = UIModalPresentationStyle.popover
         present(activityView, animated: true, completion: nil)
-        let popOver = activityView.popoverPresentationController
-        popOver?.sourceView = sender
-        popOver?.permittedArrowDirections = []
     }
     
     func presentAlert() {
@@ -218,6 +156,5 @@ class previousRoundsViewController: UIViewController, UITableViewDataSource, UIT
         alert.addAction(OKAction)
         self.present(alert, animated: true, completion: nil)
     }
-
     
 }
